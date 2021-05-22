172510c6a406b6bec1a255fb293b3934e590b81d
Mateusz <m.tretowski@wp.pl>
2021-05-22 11:56:26 +0200

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,18 @@ public class Main {
     System.out.println("ZZZ");
     System.out.println("123");
 
-    for(int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
       System.out.println(i);
     }
 
     System.out.println("XYZ");
     System.out.println("ABC");
     System.out.println("ZZZ");
+
+    int x = 9;
+    do {
+      System.out.println(x);
+      x--;
+    } while (x > 0);
   }
 }
